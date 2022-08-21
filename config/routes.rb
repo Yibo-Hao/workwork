dcfb183cd730859bbc00bbe02123d20a3a577362
yibohao <sxyhao@hotmail.com>
2022-08-21 13:47:26 +0800

    [back-end] feat: 根路由，me 路由创建

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get '/', to: 'home#index'
+
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]
       resources :session, only: [:create, :destroy]
-      resource :me, only: [:show]
+      get :me, to: 'me#show'
     end
   end
 end
